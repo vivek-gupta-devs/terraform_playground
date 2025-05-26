5aaae168a6cc87f247cb200ee9c6516c2b81e5ec
vivek gupta <vivek.gupta.devs@gmail.com>
2025-05-26 23:43:09 +0530

    terraform to create self managed vpc

---
+++ aws_vpc/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0-beta1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_subnet" "public-subnet" {
+  cidr_block = "10.0.2.0/24"
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+resource "aws_route_table" "my-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+}
+
+
+resource "aws_route_table_association" "public-sub" {
+  route_table_id = aws_route_table.my-rt.id
+  subnet_id = aws_subnet.public-subnet.id
+}
